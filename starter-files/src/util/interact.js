04795893936667f9e2ce83009678f74a6d95c466
Osama Khan <osama021khan@gmail.com>
2022-04-15 01:42:09 +0500

    feat: integrate with smart conract

--- starter-files/src/util/interact.js
+++ starter-files/src/util/interact.js
@@ -1,17 +1,104 @@
-//export const helloWorldContract;
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+const web3 = createAlchemyWeb3(alchemyKey);
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0x6f3f635A9762B47954229Ea479b4541eAF402A6A";
+
+export const helloWorldContract = new web3.eth.Contract(contractABI, contractAddress);
 
 export const loadCurrentMessage = async () => { 
-  
+  const message = await helloWorldContract.methods.message().call();
+  return message;
 };
 
 export const connectWallet = async () => {
-  
+    if (window.ethereum) {
+        try {
+          const addressArray = await window.ethereum.request({
+            method: "eth_requestAccounts",
+          });
+          const obj = {
+            status: "👆🏽 Write a message in the text-field above.",
+            address: addressArray[0],
+          };
+          return obj;
+        } catch (err) {
+          return {
+            address: "",
+            status: "😥 " + err.message,
+          };
+        }
+      } else {
+        return {
+          address: "",
+          status: ("🦊"),
+        }
+    }
 };
 
 export const getCurrentWalletConnected = async () => {
-  
+    if (window.ethereum) {
+        try {
+          const addressArray = await window.ethereum.request({
+            method: "eth_accounts",
+          });
+          if (addressArray.length > 0) {
+            return {
+              address: addressArray[0],
+              status: "👆🏽 Write a message in the text-field above.",
+            };
+          } else {
+            return {
+              address: "",
+              status: "🦊 Connect to Metamask using the top right button.",
+            };
+          }
+        } catch (err) {
+          return {
+            address: "",
+            status: "😥 " + err.message,
+          };
+        }
+      } else {
+        return {
+          address: "",
+          status: ("🦊"),
+        }
+    }
 };
 
 export const updateMessage = async (address, message) => {
-  
+    if (!window.ethereum || address === null) {
+        return {
+          status:
+            "💡 Connect your Metamask wallet to update the message on the blockchain.",
+        };
+      }
+    
+      if (message.trim() === "") {
+        return {
+          status: "❌ Your message cannot be an empty string.",
+        };
+      }
+      const transactionParameters = {
+        to: contractAddress, // Required except during contract publications.
+        from: address, // must match user's active address.
+        data: helloWorldContract.methods.update(message).encodeABI(),
+      };
+    
+    //sign the transaction
+      try {
+        const txHash = await window.ethereum.request({
+          method: "eth_sendTransaction",
+          params: [transactionParameters],
+        });
+        return {
+          status: ("💡 Your message has been updated on the blockchain. "),
+        };
+      } catch (error) {
+        return {
+          status: "😥 " + error.message,
+        };
+      }
 };
